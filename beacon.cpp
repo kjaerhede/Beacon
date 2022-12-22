678cbf23f872ec1cf9cf32ee90454b2bededcf76
Ronni Kjaerhede <77685357+kjaerhede@users.noreply.github.com>
2022-12-22 16:29:59 +0100

    add white gradiant color to loop

--- beacon.cpp
+++ beacon.cpp
@@ -42,23 +42,34 @@ void RunningLights(){
 	
   FastLED.show();
 	
-  leds[dot1]                              = CRGB::Gray;
-  leds[dot1 + NUM_RUN]                    = CRGB::Gray;
-  leds[dot1 + offset]                     = CRGB::Gray;
-  leds[dot1 + NUM_RUN + offset]           = CRGB::Gray;
-  leds[dot1 + offset*2]                   = CRGB::Gray;
-  leds[dot1 + NUM_RUN + offset*2]         = CRGB::Gray;
+  leds[dot1]                              = CRGB::GhostWhite;
+  leds[dot1 + NUM_RUN]                    = CRGB::GhostWhite;
+  leds[dot1 + offset]                     = CRGB::GhostWhite;
+  leds[dot1 + NUM_RUN + offset]           = CRGB::GhostWhite;
+  leds[dot1 + offset * 2]                 = CRGB::GhostWhite;
+  leds[dot1 + NUM_RUN + offset * 2]       = CRGB::GhostWhite;
 	
 	
-  if(dot1>0) leds[dot1 - 1]               = CRGB::Black;
-  else leds[NUM_RUN * 2 - 1]              = CRGB::Black;
-  leds[dot1 + NUM_RUN - 1]                = CRGB::Black;
-  if(dot1>0) leds[dot1 + offset - 1]      = CRGB::Black;
-  else leds[NUM_RUN * 2 + offset -1]      = CRGB::Black;
-  leds[dot1 + NUM_RUN + offset - 1]       = CRGB::Black;
-  if(dot1>0) leds[dot1 + offset * 2 - 1]  = CRGB::Black;
-  else leds[NUM_RUN * 2 + offset * 2 -1]  = CRGB::Black;
-  leds[dot1 + NUM_RUN + offset*2 - 1]     = CRGB::Black;		
+  if(dot1>0) leds[dot1 - 1]               = CRGB::Gray;
+  else leds[NUM_RUN * 2 - 1]              = CRGB::Gray;
+  leds[dot1 + NUM_RUN - 1]                = CRGB::Gray;
+  if(dot1>0) leds[dot1 + offset - 1]      = CRGB::Gray;
+  else leds[NUM_RUN * 2 + offset - 1]     = CRGB::Gray;
+  leds[dot1 + NUM_RUN + offset - 1]       = CRGB::Gray;
+  if(dot1>0) leds[dot1 + offset * 2 - 1]  = CRGB::Gray;
+  else leds[NUM_RUN * 2 + offset * 2 - 1] = CRGB::Gray;
+  leds[dot1 + NUM_RUN + offset*2 - 1]     = CRGB::Gray;		
+
+
+  if(dot1>1) leds[dot1 - 2]               = CRGB::Black;
+  else leds[NUM_RUN * 2 - 2]              = CRGB::Black;
+  leds[dot1 + NUM_RUN - 2]                = CRGB::Black;
+  if(dot1>0) leds[dot1 + offset - 2]      = CRGB::Black;
+  else leds[NUM_RUN * 2 + offset - 2]     = CRGB::Black;
+  leds[dot1 + NUM_RUN + offset - 2]       = CRGB::Black;
+  if(dot1>0) leds[dot1 + offset * 2 - 2]  = CRGB::Black;
+  else leds[NUM_RUN * 2 + offset * 2 - 2] = CRGB::Black;
+  leds[dot1 + NUM_RUN + offset*2 - 2]     = CRGB::Black;		
 
   
   delay(speed);}
